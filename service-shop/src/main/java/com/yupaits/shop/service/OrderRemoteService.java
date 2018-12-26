3425cf2a6b4acca4f55be6f46ae1e863a224279b
tangsheng <ts@posun.cn>
2018-12-26 18:29:39 +0800

    修正FeignClient

--- service-shop/src/main/java/com/yupaits/shop/service/OrderRemoteService.java
+++ service-shop/src/main/java/com/yupaits/shop/service/OrderRemoteService.java
@@ -1,17 +1,18 @@
 package com.yupaits.shop.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
 
 /**
  * @author yupaits
  * @date 2018/12/26
  */
-@FeignClient(name = "order-service")
+@FeignClient(name = "service-order")
 public interface OrderRemoteService {
 
     @GetMapping("/{orderId}")
-    ResponseEntity getOrderById(@PathVariable String orderId);
+    Map getOrderById(@PathVariable String orderId);
 }
